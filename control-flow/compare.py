ca703126706867ac7d632fe4a6fe9bd0e6f9f3a1
Callan <callan.v@fake.com>
2023-10-05 14:38:01 +1100

    added lesson file

---
+++ control-flow/compare.py
@@ -0,0 +1,56 @@
+# x = int(input("What is X? "))
+# y = int(input("What is Y? "))
+
+# if x < y:
+#     print(f"{x} is more than {y}")
+# elif x == y:
+#     print("Welp, looks like they're the same number!")
+# else:
+#     print(f"{y} is more than {x}")
+
+# print("Okay, that's all folks. Go home.")
+
+# score = int(input("Score: "))
+
+
+
+# if score >= 90:
+#     print("HD")
+# elif score >= 80:
+#     print("D")
+# elif score >= 70:
+#     print("CR")
+# elif score >= 50:
+#     print("P")
+# else:
+#     print("F")
+
+
+
+# name = input("Name: ")
+
+# match name:
+#     case "Harry" | "Ron" | "Hermoine":
+#         print("Gryffindor")
+#     case "Draco":
+#         print("Slytherin") 
+#     case _:
+#         print("Mudblood!") 
+
+
+
+
+shape = input("Square, Triangle, or Circle? ")
+print("What's the size?")
+length = int(input(" "))
+
+if shape == "Square":
+    print(f"Your {shape}'s area is {length ** 2}")
+elif shape == "Triangle":
+    height= int(input())
+    
+
+# elif shape == "Triangle":
+#     print(f"Your {shape}'s area is {size}")
+#     case "Triangle":
+#         print()
