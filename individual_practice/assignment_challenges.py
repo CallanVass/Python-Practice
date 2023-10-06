3539ae3ab6c8bc162a77ecd34df64bcfed4c7c76
Callan <callan.v@fake.com>
2023-10-07 09:08:40 +1100

    tried to answer question, found some sources that might help

---
+++ individual_practice/assignment_challenges.py
@@ -0,0 +1,27 @@
+# arr = [5, 22, 29, 39, 19, 51, 78, 96, 84]
+# i = 0
+# while (i<arr.len( -1) and (arr[i] < arr[i+1])):
+#     i += i
+#     print(i)
+#     arr[i] = arr[i+1]
+#     arr[i + 1] = arr[i]
+
+# arr = [5, 22, 29, 39, 19, 51, 78, 96, 84]
+# i = 0
+# while (i < len(arr) -1) and (arr[i] < arr[i+1]):
+#     i += i
+#     print(i)
+#     arr[i] = arr[i+1]
+#     arr[i + 1] = arr[i]
+#     print(arr)
+
+# arr = [5, 22, 29, 39, 19, 51, 78, 96, 84]
+# i = 0
+# while i < len(arr)-1 and (arr[i] < arr[i+1]):
+#     print(i)
+#     arr[i], arr[i + 1] = arr[i + 1], arr[i]
+#     print(arr)
+#     i += 1
+
+
+    
