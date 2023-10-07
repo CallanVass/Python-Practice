98eb297cabc95706a897af261d9fda0265488c02
Callan <callan.v@fake.com>
2023-10-07 16:39:35 +1100

    Completed and refined weather function for assignment

--- individual_practice/assignment_challenges.py
+++ individual_practice/assignment_challenges.py
@@ -32,31 +32,49 @@
 #     arr[i + 1] = arr[i]
 #     print(arr)
 
+#COMPLETED CODE SNIPPET TO ORGANISE AN UNORGANISE LIST INTO ASCENDING ORDER
 
 
+# # Original list
+# arr = [5, 22, 29, 39, 19, 51, 78, 96, 84]
+# # Declaring variable for iteration
+# i = 0
+# # While loop using the len() function to determine if i is less than the last value
+# while i < len(arr) - 1:
+#     #If i is i is greater than the next value
+#     if arr[i] > arr[i + 1]:
+#         #Declaring placeholder value to store i for later use
+#         place_holder = arr[i]
+#         #Swapping i with the value to the right (i + 1)
+#         arr[i] = arr[i + 1]
+#         #Taking the swapped value and placing it where i used to be
+#         arr[i + 1] = place_holder
+#         #Print which numbers are swapping, along with the element's position
+#         print(f"Swapping {arr[i]} (element number {i}) with {arr[i + 1]}")
+#         #Printing array for reference
+#         print(arr)
+#         #Returning to the first element to check if there are more elements to swap
+#         i = 0
+#     #If the if statement doesn't meet it's conditions, add one to i (to continue iteration)
+#     else: 
+#         i += 1
 
-# Original list
-arr = [5, 22, 29, 39, 19, 51, 78, 96, 84]
-# Declaring variable for iteration
-i = 0
-# While loop using the len() function to determine if i is less than the last value
-while i < len(arr) - 1:
-    #If i is i is greater than the next value
-    if arr[i] > arr[i + 1]:
-        #Decalring placeholder value to store i for later use
-        place_holder = arr[i]
-        #Swapping i with the value to the right (i + 1)
-        arr[i] = arr[i + 1]
-        #Taking the swapped value and placing it where i used to be
-        arr[i + 1] = place_holder
-        #Print which numbers are swapping, along with the element's position
-        print(f"Swapping {arr[i]} (element number {i}) with {arr[i + 1]}")
-        #Printing array for reference
-        print(arr)
-        #Returning to the first element to check if there are more elements to swap
-        i = 0
-    #If the if statement doesn't meet it's conditions, add one to i (to continue iteration)
-    else: 
-        i += 1
+
+#WEATHER CHALLENGE
+
+
+def weather_function(rain, temp):
+    if rain:
+        if temp < 15:
+            return "It's wet and cold"
+        else:
+            return"It's warm and raining"
+    else:
+        if temp < 15:
+            return "It's not raining but cold"
+        else:
+            return "It's warm but not raining"
+        
+print(weather_function(True, 14))
 
 
