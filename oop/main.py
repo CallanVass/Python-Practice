572a63a3babde2468aac65a8db2aa0e7a9f1829d
Callan <callan.v@fake.com>
2023-10-17 08:21:03 +1100

    coded first part of a tic tac toe game

--- oop/main.py
+++ oop/main.py
@@ -51,9 +51,9 @@ from oop import Book
 #Player objects
 import random
 
-game_matrix = [["", "", ""],
-                ["", "", ""],
-                ["", "", ""]]
+game_matrix = [["1", "2", "3"],
+                ["4", "5", "6"],
+                ["7", "8", "9"]]
 
 winning_dict = {
     "Player_1_wins": 0,
@@ -64,4 +64,69 @@ print("Welcome to TICK TACK TOE!")
 player_1 = input("Please enter the name of the first Player: ")
 player_2 = input("Please enter the second Player: ")
 
-random_player_choice = random.choice(player_1, player_2)
+
+
+print(f"{player_1} will be going first! They are 'x'")
+
+
+for line in game_matrix:
+    print(line)
+
+
+
+def player_1_loop():
+    anchor = True
+    while anchor:
+        move = input("Please enter the number you wish to place your mark on: ")
+        match move:
+            case "1":
+                game_matrix[0][0] = "X"
+                for line in game_matrix:
+                    print(line)
+                return
+            case "2":
+                game_matrix[0][1] = "X"
+                for line in game_matrix:
+                    print(line)
+                return
+            case "3":
+                game_matrix[0][2] = "X"
+                for line in game_matrix:
+                    print(line)
+                return
+            case "4":
+                game_matrix[1][0] = "X"
+                for line in game_matrix:
+                    print(line)
+                return
+            case "5":
+                game_matrix[1][1] = "X"
+                for line in game_matrix:
+                    print(line)
+                return
+            case "6":
+                game_matrix[1][2] = "X"
+                for line in game_matrix:
+                    print(line)
+                return
+            case "7":
+                game_matrix[2][0] = "X"
+                for line in game_matrix:
+                    print(line)
+                return
+            case "8":
+                game_matrix[2][1] = "X"
+                for line in game_matrix:
+                    print(line)
+                return
+            case "9":
+                game_matrix[2][2] = "X"
+                for line in game_matrix:
+                    print(line)
+                return
+player_1_loop()
+
+
+
+
+#Only have 1 go per player
